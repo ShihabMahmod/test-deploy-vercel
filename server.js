ba921f825d45aa5cdd682e3104d048f834fbd35c
ShihabMahmod <shihabmahmod58@gmail.com>
2024-05-20 04:48:01 +0600

    deployment test3

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+  });
+  
 app.listen(500, () => console.log(`Server is running on PORT 500`));
